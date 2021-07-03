17911b7f82e40501e57b63f3c780b79f7db1858d
Madhur Dheer <maddheer@amazon.com>
2021-07-03 17:45:26 +0530

    Android Support for the App

--- android/app/src/main/java/com/citybubbleproject/MainApplication.java
+++ android/app/src/main/java/com/citybubbleproject/MainApplication.java
@@ -4,16 +4,11 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.oblador.vectoricons.VectorIconsPackage;
 import java.lang.reflect.InvocationTargetException;
-import com.horcrux.svg.SvgPackage;
 import java.util.List;
 
 
@@ -29,10 +24,10 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages(),SvgPackage().getPackages(),new VectorIconsPackage().getPackages();
-          
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          //packages.add(new VectorIconsPackage());
           return packages;
         }
 
